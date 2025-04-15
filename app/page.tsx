7b36d2acc09c06bd1fac7cbbed102898499932e9
{shiwamanam} <shiwamanami.adesign@gmail.com>
2025-04-15 20:10:06 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,8 @@ export default function LandingPage() {
             fill
             className="object-cover"
             priority
+            quality={75}
+            sizes="100vw"
           />
         </div>
         <Header />
@@ -87,11 +89,11 @@ export default function LandingPage() {
           {/* What's Transparent LED Film */}
           <section className="container max-w-6xl py-10 md:py-20 mb-10">
             <h2>
-              What's Transparent LED Film ?<span>透明LEDフィルムとは？</span>
+              What's this ?<span>透明LEDフィルムとは？</span>
             </h2>
 
-            <div className="flex gap-10 mb-24">
-              <div className="w-1/2">
+            <div className="flex flex-col md:flex-row gap-10 mb-16 md:mb-24">
+              <div className="w-full md:w-1/2">
                 <ImageSlider
                   images={[
                     "/images/about/01.jpg",
@@ -100,24 +102,33 @@ export default function LandingPage() {
                   ]}
                 />
               </div>
-              <div className="w-1/2">
-                <p className="text-4xl font-bold mb-10">
-                  革新的なディスプレイ技術
+              <div className="w-full md:w-1/2">
+                <p className="text-3xl md:text-4xl font-bold !leading-normal mb-5 md:mb-10">
+                  革新的な
+                  <br className="xs:hidden" />
+                  ディスプレイ技術
                 </p>
-                <p className="text-lg">
-                  透明LEDフィルムは、ガラスやアクリルなどの透明素材に貼り付けて使用する最先端のディスプレイ技術です。
-                  <br />
-                  これにより、空間デザインを損なうことなく、鮮やかな映像を表示でき、視界を遮ることなく、広告やインフォメーションを発信できます。
-                  <br />
-                  この技術は、空間を開放的に保ちながらも、強力な訴求力を持つ映像を提供します。
-                  <br />
-                  昼と夜で異なる印象を与えることができるのも魅力です。
-                </p>
+                <div className="[&_p]:text-base md:[&_p]:text-lg">
+                  <p className="mb-5 md:mb-10">
+                    透明LEDフィルムは、ガラスやアクリルなどの透明素材に貼り付けて使用する最先端のディスプレイ技術です。
+                  </p>
+                  <p className="mb-5 md:mb-10">
+                    これにより、空間デザインを損なうことなく、鮮やかな映像を表示でき、視界を遮ることなく、広告やインフォメーションを発信できます。
+                  </p>
+                  <p>
+                    この技術は、空間を開放的に保ちながらも、強力な訴求力を持つ映像を提供します。
+                    <br />
+                    昼と夜で異なる印象を与えることができるのも魅力です。
+                  </p>
+                </div>
               </div>
             </div>
 
             <div className="flex justify-center">
-              <Button>透明LEDフィルムについて詳しく知る</Button>
+              <Button>
+                {mainData.keyWords} について詳しく知る
+                <ArrowRight className="h-4 w-4" />
+              </Button>
             </div>
           </section>
 
@@ -127,7 +138,7 @@ export default function LandingPage() {
               Benefits<span>導入のメリット</span>
             </h2>
 
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-5 text-black mb-20 md:mb-24">
+            <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-5 text-black mb-16 md:mb-24">
               <div className="relative mx-auto w-full max-w-xs sm:max-w-sm md:max-w-md bg-[#f1f1f1] py-4 md:py-6 px-4 md:px-8">
                 <p
                   className="relative inline-block box-border font-semibold w-[calc(100%+20px)] ml-[-3.3rem] px-5 py-2.5 text-lg sm:text-lg md:text-xl bg-gray-700 text-yellow-300 text-center mb-5
@@ -263,21 +274,22 @@ export default function LandingPage() {
 
             <div className="flex justify-center">
               <Link href="/services">
-                <Button variant="default">
-                  透明LEDフィルムについて詳しく知る
+                <Button>
+                  {mainData.keyWords} について詳しく知る
+                  <ArrowRight className="h-4 w-4" />
                 </Button>
               </Link>
             </div>
           </section>
 
           {/* Case Studies */}
-          <section className="container max-w-6xl bg-white/15 px-5 md:px-10 py-10 md:py-20 mb-10">
+          <section className="container max-w-6xl bg-white/15 px-5 md:px-10 py-14 md:py-20 mb-10">
             <h2>
               Case Studies
               <span>活用事例</span>
             </h2>
 
-            <div className="grid gap-5 grid-cols-1 xs:grid-cols-2 md:grid-cols-3 mb-20">
+            <div className="grid gap-5 grid-cols-1 xs:grid-cols-2 md:grid-cols-3 mb-16 md:mb-24">
               {cases.slice(0, 3).map((case_) => (
                 <div key={case_.id} className="">
                   <div className=" flex items-center justify-center mb-3 md:mb-5">
@@ -301,7 +313,7 @@ export default function LandingPage() {
             <div className="flex justify-center">
               <Link href="/cases">
                 <Button variant="default">
-                  活用事例を詳しく見る
+                  活用事例について詳しく見る
                   <ArrowRight className="h-4 w-4" />
                 </Button>
               </Link>
@@ -318,7 +330,7 @@ export default function LandingPage() {
             <div className="flex justify-center">
               <Link href="/blog">
                 <Button variant="default">
-                  製品ラインナップを詳しく見る
+                  製品ラインナップについて詳しく見る
                   <ArrowRight className="h-4 w-4" />
                 </Button>
               </Link>
@@ -333,44 +345,43 @@ export default function LandingPage() {
             </h2>
 
             <div className="mb-24 md:mb-32">
-              <div className="flex gap-5 md:gap-10 mb-5 items-end relative">
-                <div className="w-full md:w-2/3 relative aspect-[3/4] md:aspect-video flex items-start">
-                  <picture>
-                    <source
-                      media="(max-width: 767px)"
-                      srcSet="/images/about/01_sp.jpg"
-                    />
-                    <img
-                      src="/images/about/01.jpg"
-                      alt=""
-                      className="w-full h-full object-cover"
-                    />
-                  </picture>
+              <div className="relative flex flex-col lg:flex-row items-stretch">
+                <div className="w-full lg:w-1/2">
+                  <Image
+                    src="/images/about/01.jpg"
+                    alt="LED Display"
+                    width={800}
+                    height={600}
+                    className="object-cover w-full h-full"
+                  />
                 </div>
-                <div className="w-auto">
-                  <div className="flex items-baseline mb-8 md:mb-14">
-                    <p className="text-yellow-300 text-4xl md:text-6xl font-bold leading-none">
-                      販売
+
+                <div className="w-full lg:w-1/2 relative -z-10 px-8 md:pt-20">
+                  <div className="absolute top-0 left-0 right-0 bottom-0 w-screen bg-white/20 -z-10 translate-x-4 -translate-y-4 lg:-translate-x-20 lg:translate-y-10"></div>
+                  <div className="relative">
+                    <div className="flex items-baseline mb-8 md:mb-14">
+                      <h3 className="text-yellow-300 text-4xl md:text-5xl font-bold leading-none">
+                        販売
+                      </h3>
+                      <p className="text-yellow-300/80 text-xl md:text-3xl font-bold leading-none ml-2">
+                        Sales
+                      </p>
+                    </div>
+                    <p className="mb-10 md:mb-14">
+                      屋内・屋外用のLEDビジョンを幅広く取り扱っています。
+                      <br />
+                      シースルービジョンや高解像度屋内LEDビジョン、一体型LEDビジョンなど、様々なニーズに応える最先端の製品と、最適な映像ソリューションをご提案しています。
                     </p>
-                    <p className="text-yellow-300/80 text-xl md:text-3xl font-bold leading-none ml-2">
-                      Sales
-                    </p>
-                  </div>
-                  <p className="md:pr-40 mb-10 md:mb-14">
-                    屋内・屋外用のLEDビジョンを幅広く取り扱っています。
-                    <br />
-                    シースルービジョンや高解像度屋内LEDビジョン、一体型LEDビジョンなど、様々なニーズに応える最先端の製品と、最適な映像ソリューションをご提案しています。
-                  </p>
-                  <div>
-                    <Link href="/services">
-                      <Button variant="default">
-                        販売について詳しく見る
-                        <ArrowRight className="h-4 w-4" />
-                      </Button>
-                    </Link>
+                    <div>
+                      <Link href="/services">
+                        <Button variant="default">
+                          販売について詳しく見る
+                          <ArrowRight className="h-4 w-4" />
+                        </Button>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-                <div className="absolute top-16 right-0 md:-bottom-12 left-auto md:w-2/3 h-full bg-white/20 -z-10" />
               </div>
             </div>
 
@@ -431,9 +442,9 @@ export default function LandingPage() {
               <span>導入の流れ</span>
             </h2>
 
-            <div className="grid gap-12 md:gap-8 grid-cols-1 md:grid-cols-3 pt-14 mb-10">
+            <div className="grid gap-10 md:gap-8 grid-cols-1 md:grid-cols-3 pt-14 mb-10">
               <div className="relative">
-                <p className="absolute -top-7 left-0 text-yellow-300 text-4xl font-bold">
+                <p className="absolute -top-[1.4rem] md:-top-7 left-0 text-yellow-300 text-3xl md:text-4xl font-bold">
                   Step 1
                 </p>
                 <div className="absolute top-5 right-5 md:right-8">
@@ -442,7 +453,7 @@ export default function LandingPage() {
                     alt=""
                     width={90}
                     height={90}
-                    layout="intrinsic"
+                    className="object-contain w-20 md:w-24 h-20 md:h-24"
                     priority
                   />
                 </div>
@@ -457,7 +468,7 @@ export default function LandingPage() {
               </div>
 
               <div className="relative">
-                <p className="absolute -top-7 left-0 text-yellow-300 text-4xl font-bold">
+                <p className="absolute -top-[1.4rem] md:-top-7 left-0 text-yellow-300 text-3xl md:text-4xl font-bold">
                   Step 2
                 </p>
                 <div className="absolute top-5 right-5 md:right-8">
@@ -466,7 +477,7 @@ export default function LandingPage() {
                     alt=""
                     width={90}
                     height={90}
-                    layout="intrinsic"
+                    className="object-contain w-20 md:w-24 h-20 md:h-24"
                     priority
                   />
                 </div>
@@ -479,7 +490,7 @@ export default function LandingPage() {
               </div>
 
               <div className="relative">
-                <p className="absolute -top-7 left-0 text-yellow-300 text-4xl font-bold">
+                <p className="absolute -top-[1.4rem] md:-top-7 left-0 text-yellow-300 text-3xl md:text-4xl font-bold">
                   Step 3
                 </p>
                 <div className="absolute top-5 right-5 md:right-8">
@@ -488,7 +499,7 @@ export default function LandingPage() {
                     alt=""
                     width={90}
                     height={90}
-                    layout="intrinsic"
+                    className="object-contain w-20 md:w-24 h-20 md:h-24"
                     priority
                   />
                 </div>
@@ -512,12 +523,12 @@ export default function LandingPage() {
                     "linear-gradient(to right, #000000 0%, #000000 2%, #14532d 40%, #facc15 100%)",
                 }}
               ></div>
-              <span className="z-10 text-xl md:text-2xl tracking-wider">
+              <span className="z-10 text-xl md:text-2xl tracking-wider px-2">
                 ご注文から最短20日で導入完了！
               </span>
             </div>
 
-            <p className="text-center mb-24 md:mb-32">
+            <p className="text-center mb-16 md:mb-32">
               ※導入完了後も安心してお使いいただけるよう、製品には2年間の保証をお付けしています。
             </p>
 
