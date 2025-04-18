83c7957c5e5b7494a9df98d5d5e4c38f0b11364e
{shiwamanam} <shiwamanami.adesign@gmail.com>
2025-04-18 13:10:02 +0900

    fix: add component exports

---
+++ components/ui/index.ts
@@ -0,0 +1,23 @@
+import Button from "./Button";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "./Accordion";
+import { Cta } from "./Cta";
+import { ImageSlider } from "./ImageSlider";
+import LogoSlider from "./LogoSlider";
+import { Tabs } from "./Tabs";
+
+export {
+  Button,
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+  Cta,
+  ImageSlider,
+  LogoSlider,
+  Tabs,
+};
