6ed558885c7676252c2d57458bdf8ddcb45da853
{shiwamanam} <shiwamanami.adesign@gmail.com>
2025-05-02 16:27:05 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,9 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
           `}
         </Script>
       </head>
